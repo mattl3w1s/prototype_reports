44580eb4ec59d9c090d6f72223472632f99f4fd3
mattl3w1s <matt@csiga.org>
2018-02-20 09:34:55 -0600

    Added jinja filter to trim (lecture) from course names.

--- processors/generate_html_report.py
+++ processors/generate_html_report.py
@@ -25,9 +25,13 @@ def sanjacify(course):
     else:
         return course
 
+def trim(string_):
+    return string_.replace(' (lecture)','')
+
 
 env = Environment(loader=FileSystemLoader('.'))
 env.filters["sanjacify"] = sanjacify
+env.filters["trim"] = trim
 template = env.from_string(template_file)
 
 programs = session.query(Program).order_by(Program.name)
